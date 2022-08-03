3937d8549ea207e94adcebdfbd7a228013eebcc5
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2022-08-03 10:31:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 29 15:14:21 2022
+Created on Mon Aug  1 18:29:51 2022
 
 @author: erikb
 """
@@ -10,7 +10,78 @@ import numpy as np # load numpy for NaN substition
 #import json
 import streamlit as st
 
-fields = ["Customer Search Term","Spend","14 Day Total Sales","Campaign Name"]
+fields = ["Match Type","Customer Search Term","Clicks","Spend","14 Day Total Sales","Campaign Name","14 Day Total Orders (#)"]
+
+st.title('KW Data Summary')
+st.sidebar.title('Search Query report analysis:')
+st.sidebar.write('Please upload the Search query report for analysis')
+st.sidebar.write('Note: this should include the fields: Customer Search Term, Spend, 14 Day Total Sales, Campaign Name, 14 Day Total Orders (\#)')
+
+pw = "pass123"
+
+# Upload data to analyse
+uploaded_file = st.sidebar.file_uploader("Upload Search Query File")
+
+if uploaded_file is not None and pw == "pass123":
+    #read csv
+    df=pd.read_csv(uploaded_file)
+    df2 = df.copy()
+    st.write("csv successfully uploaded")
+    df = df[fields]
+    
+    cols = ['Match Type','Query','Clicks','Cost','Revenue','Campaign Name','Orders']
+    df.columns = cols
+    
+    df = df.sort_values(by=['Cost'], ascending=False)
+    df['ROAS'] = df['Revenue']/df['Cost']
+    df['CTR'] = df['Orders']/df['Clicks']
+    
+    brands = 'finish|vanish|air wick|calgon|airwick|cillit'
+    df.loc[df['Query'].str.contains(brands),'BrandKW'] = "Branded"
+    df['BrandKW'].fillna("Non Branded", inplace=True)
+    
+    desc = {"Vanish":"Vanish","Finish": "Finish", "Calgon": "Calgon", "Air Wick": "Air Wick", "Botanica": "Botanica", "Cillit" : "Cillit Bang", "Mr Sheen": "Mr Sheen"}
+
+    # add brand column
+    def check_kw(x):
+        for key in desc:
+            if key.lower() in x.lower():
+                return desc[key]
+        return ''
+    
+    df["Brand"] = df["Campaign Name"].map(lambda x: check_kw(x))
+    df['Brand'].replace("","Other", inplace=True)
+    
+    #get brand level summary table
+    st.write("Brand performance summary table")
+    dfBrand = pd.pivot_table(df,index=['Brand'],values=['Cost','Revenue','Orders','Clicks'], aggfunc=np.sum)
+    dfBrand.reset_index(inplace=True)
+    dfBrand['ROAS'] = dfBrand['Revenue']/dfBrand['Cost']
+    dfBrand['AOV'] = dfBrand['Revenue']/dfBrand['Orders']
+    dfBrand['CVR'] = dfBrand['Orders']/dfBrand['Clicks']
+    st.dataframe(dfBrand)
+    
+    st.write("Brand/ Non Brand performance summary table")
+    # Brand non brand summary
+    dfBNonB = pd.pivot_table(df,index=['BrandKW'],values=['Cost','Revenue','Orders','Clicks'], aggfunc=np.sum)
+    dfBNonB.reset_index(inplace=True)
+    dfBNonB['ROAS'] = dfBNonB['Revenue']/dfBNonB['Cost']
+    dfBNonB['AOV'] = dfBNonB['Revenue']/dfBNonB['Orders']
+    dfBNonB['CVR'] = dfBNonB['Orders']/dfBNonB['Clicks']
+    st.dataframe(dfBNonB)
+    
+    # Match type level performance
+    st.write("Match Type performance summary table")
+    dfBNonB = pd.pivot_table(df,index=['Match Type'],values=['Cost','Revenue','Orders','Clicks'], aggfunc=np.sum)
+    dfBNonB.reset_index(inplace=True)
+    dfBNonB['ROAS'] = dfBNonB['Revenue']/dfBNonB['Cost']
+    dfBNonB['AOV'] = dfBNonB['Revenue']/dfBNonB['Orders']
+    dfBNonB['CVR'] = dfBNonB['Orders']/dfBNonB['Clicks']
+    st.dataframe(dfBNonB)
+    
+# Negative to Add functions
+
+fields2 = ["Customer Search Term","Spend","14 Day Total Sales","Campaign Name"]
 
 # function to find substrings for a given campaign name
 def findsubstrings(c):
@@ -51,27 +122,27 @@ def clean_negatives(dfc):
     return(dfc)
 
 st.title('Negative Substring analysis')
-st.write('Please upload the Search query report for analysis')
-st.write('Note: this should include the fields: Customer Search Term, Spend, 14 Day Total Sales, Campaign Name')
+#st.write('Please upload the Search query report for analysis')
+#st.text('Note: this should include the fields: Customer Search Term, Spend, 14 Day Total Sales, Campaign Name')
 
 # Upload data to analyse
-uploaded_file = st.file_uploader("Upload Search Query File")
+#uploaded_file = st.file_uploader("Upload Search Query File")
 
 if uploaded_file is not None:
     #read csv
-    df=pd.read_csv(uploaded_file)
+    #dfN=pd.read_csv(uploaded_file)
     st.write("csv successfully uploaded")
-    df = df[fields]
+    dfN = df2[fields2]
     #st.dataframe(df)
 
     #cols = ['Campaign Name','Query','Cost','Revenue']
     cols = ['Query','Cost','Revenue','Campaign Name']
-    df.columns = cols
+    dfN.columns = cols
     #df.head()
 
     # analysis should be run at a campaign level
     # create a set of campaign names
-    campaigns = set(df["Campaign Name"])
+    campaigns = set(dfN["Campaign Name"])
 
     # initialise list to hold all substring/ campaign combinations
     results_list = []
@@ -79,7 +150,7 @@ if uploaded_file is not None:
     # for each campaign find a list of substrings 
     with st.spinner("calculating..."):
         for c in campaigns:
-            dfc = df[df['Campaign Name'] == c]
+            dfc = dfN[dfN['Campaign Name'] == c]
             kwlist = dfc['Query'].tolist()
     
             # find all substrings in specific campaign
@@ -125,3 +196,6 @@ if uploaded_file is not None:
           "text/csv",
           key='download-csv'
         )
+    
+# Broad and phrase to exact match analysis
+st.title('Broad/ Phrase to Exact Match')
